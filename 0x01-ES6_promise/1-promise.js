dfac2098687e23f7a5516bb72b7192791a544320
Noah Too <krivahtoo@gmail.com>
2024-10-02 08:45:02 +0300

    Fix 0x01. ES6 Promises task

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,12 +1,10 @@
 export default function getFullResponseFromAPI(success) {
-  return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({
-        status: 200,
-        body: 'Success'
-      });
-    } else {
-      reject(new Error('The fake API is not working currently'));
-    }
-  });
+  if (success) {
+    return Promise.resolve({
+      status: 200,
+      body: 'Success'
+    });
+  } else {
+    return Promise.reject(new Error('The fake API is not working currently'));
+  }
 }
--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,5 +3,7 @@ import { createUser, uploadPhoto } from './utils.js';
 export default function handleProfileSignup() {
   Promise.all([createUser(), uploadPhoto()]).then(([user, status]) => {
     console.log(`${status.body} ${user.firstName} ${user.lastName}`);
+  }).catch(() => {
+    console.log('Signup system offline');
   })
 }
