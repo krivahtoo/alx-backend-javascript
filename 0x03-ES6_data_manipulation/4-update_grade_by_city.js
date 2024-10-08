6182881c9877036cf232b294fe35507a4b37c340
Noah Too <krivahtoo@gmail.com>
2024-10-08 12:22:13 +0300

    Add 0x03. ES6 data manipulation tasks 0-4

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,6 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students.filter((student) => student.location === city).map((student) => {
+    const grade = newGrades.find((obj) => obj.studentId === student.id);
+    return { ...student, grade: grade ? grade.grade : 'N/A' };
+  });
+}
