aeb07893d60cfa35a820a1911c9d725824e819ef
Noah Too <krivahtoo@gmail.com>
2025-01-13 13:56:00 +0300

    Add Tasks 5. Hooks

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,26 @@
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  let consoleSpy;
+
+  beforeEach(() => {
+    consoleSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    consoleSpy.restore();
+  });
+
+  it('should log "The total is: 120" when inputs are 100 and 20', () => {
+    sendPaymentRequestToApi(100, 20);
+    sinon.assert.calledOnce(consoleSpy);
+    sinon.assert.calledWithExactly(consoleSpy, 'The total is: 120');
+  });
+
+  it('should log "The total is: 20" when inputs are 10 and 10', () => {
+    sendPaymentRequestToApi(10, 10);
+    sinon.assert.calledOnce(consoleSpy);
+    sinon.assert.calledWithExactly(consoleSpy, 'The total is: 20');
+  });
+});
