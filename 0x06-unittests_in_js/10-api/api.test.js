64414a490b9430b560d9c54ae8a94f999aa992ac
Noah Too <krivahtoo@gmail.com>
2025-01-13 20:01:21 +0300

    Add Tasks 10. Deep equality & Post integration testing

---
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -0,0 +1,81 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('Index page', () => {
+  const baseUrl = 'http://localhost:7865/';
+
+  it('should return status code 200', (done) => {
+    request.get(baseUrl, (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      done(error);
+    });
+  });
+
+  it('should return the correct message', (done) => {
+    request.get(baseUrl, (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
+      done(error);
+    });
+  });
+
+  it('should return a response with the correct content type', (done) => {
+    request.get(baseUrl, (error, response, body) => {
+      expect(response.headers['content-type']).to.include('text/html');
+      done(error);
+    });
+  });
+});
+
+describe('Cart page', () => {
+  const cartBaseUrl = 'http://localhost:7865/cart/';
+
+  it('should return status code 200 for a valid cart id', (done) => {
+    request.get(`${cartBaseUrl}12`, (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Payment methods for cart 12');
+      done(error);
+    });
+  });
+
+  it('should return status code 404 for an invalid cart id', (done) => {
+    request.get(`${cartBaseUrl}hello`, (error, response, body) => {
+      expect(response.statusCode).to.equal(404);
+      done(error);
+    });
+  });
+});
+
+
+describe('Available payments', () => {
+  const paymentsUrl = 'http://localhost:7865/available_payments';
+
+  it('should return status code 200 and the correct object', (done) => {
+    request.get(paymentsUrl, (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(JSON.parse(body)).to.deep.equal({
+        payment_methods: {
+          credit_cards: true,
+          paypal: false,
+        },
+      });
+      done(error);
+    });
+  });
+});
+
+describe('Login', () => {
+  const loginUrl = 'http://localhost:7865/login';
+
+  it('should return status code 200 and the correct message', (done) => {
+    const options = {
+      url: loginUrl,
+      method: 'POST',
+      json: { userName: 'Betty' },
+    };
+    request(options, (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome Betty');
+      done(error);
+    });
+  });
+});
