59c2b35e3aad2dd06effb3a5d67492aacd39095a
Noah Too <krivahtoo@gmail.com>
2025-01-13 12:31:34 +0300

    Add Tasks 0-1 Basic test with Mocha and Node assertion library

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,32 @@
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', () => {
+  describe('SUM', () => {
+    it('should return 6 when inputs are 1.4 and 4.5', () => {
+      assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+    });
+  });
+
+  describe('SUBTRACT', () => {
+    it('should return -4 when inputs are 1.4 and 4.5', () => {
+      assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+    });
+  });
+
+  describe('DIVIDE', () => {
+    it('should return 0.2 when inputs are 1.4 and 4.5', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+    });
+
+    it('should return "Error" when dividing by 0', () => {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+    });
+  });
+
+  describe('Invalid type', () => {
+    it('should throw an error when type is invalid', () => {
+      assert.throws(() => calculateNumber('MULTIPLY', 1.4, 4.5), /Invalid type/);
+    });
+  });
+});
